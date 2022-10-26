29c283623c79c56bd50eb058a699cb84db404f35
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2022-10-26 14:50:38 +0200

    Clean op : onsdag

--- FoodHub-Web-Client/src/app/app-routing.module.ts
+++ FoodHub-Web-Client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product/:id', component: ProductComponent },
   { path:'login', component: LoginformComponent },
+  
   { path: '**', redirectTo: ''} // MUST BE LAST IN ARRAY!
 ];
 
