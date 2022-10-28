7509ea1d980309d03c24a92c9d4eca5f3af9490e
Matthias Skov Bryde <73159216+MatthiasSkovBryde@users.noreply.github.com>
2022-10-28 13:04:56 +0200

    Revert "Matthias dev"

--- FoodHub Web API/DTOs/RefreshToken/DirectRefreshTokenResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoodHub_Web_API.DTOs.RefreshToken
-{
-    public class DirectRefreshTokenResponse
-    {
-        public string Token { get; set; } = string.Empty;
-
-        public DateTime Expires_At { get; set; }
-    }
-}
--- FoodHub-Web-Client/src/app/app.module.ts
+++ FoodHub-Web-Client/src/app/app.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ProductComponent } from './components/product/product.component';
-import { LoginformComponent } from './components/loginform/loginform.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    ProductComponent,
-    LoginformComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
