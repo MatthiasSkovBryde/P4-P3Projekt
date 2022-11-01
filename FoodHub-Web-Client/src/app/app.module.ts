17249f4972b579d57688d6d150a47bb012875354
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2022-11-01 09:20:01 +0100

    clean

--- FoodHub-Web-Client/src/app/app.module.ts
+++ FoodHub-Web-Client/src/app/app.module.ts
@@ -10,9 +10,6 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductComponent } from './components/product/product.component';
 import { LoginComponent } from './components/login/login.component';
-import { appInitializer } from './helpers/app.initializer';
-import { AuthenticationService } from './_services/authentication.service';
-import { AuthenticationInterceptor } from './_interceptor/authentication.interceptor';
 import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -30,9 +27,6 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   providers: [
-    { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AuthenticationService]},
-    { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true},
-    { provide: LocationStrategy, useClass: HashLocationStrategy, multi: true},
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'DKK'},
     { provide: LOCALE_ID, useValue: 'en-DK'}
   ],
