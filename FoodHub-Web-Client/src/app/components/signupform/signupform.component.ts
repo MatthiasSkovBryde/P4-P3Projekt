a287aa90d7625eaa6f4b48e47955b8fb1b18168b
Haittham <hait0029@gmail.com>
2022-11-03 11:57:56 +0100

    icons

--- FoodHub-Web-Client/src/app/components/signupform/signupform.component.ts
+++ FoodHub-Web-Client/src/app/components/signupform/signupform.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signupform.component.css']
 })
 export class SignupformComponent implements OnInit {
+  visible: boolean = true;
+  changetype: boolean = true;
 
+  viewpass() {
+    this.visible = !this.visible;
+    this.changetype = !this.changetype;
+  }
   constructor() { }
 
   ngOnInit(): void {
