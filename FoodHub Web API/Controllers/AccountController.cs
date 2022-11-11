d8412155f3daddb26edfe447f048b98d42b05727
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2022-11-11 08:13:38 +0100

    added accountService and accountController

---
+++ FoodHub Web API/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿namespace FoodHub_Web_API.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                List<StaticAccountResponse> accounts = await _accountService.GetAll();
+
+                if (accounts == null)
+                {
+                    return Problem("Nothing was returned from service, this was unexpected");
+                }
+
+                if (accounts.Count == 0)
+                {
+                    return NoContent();
+                }
+
+                return Ok(accounts);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{accountId")]
+        public async Task<IActionResult> GetById(int accountId)
+        {
+            try
+            {
+                DirectAccountResponse directAccountResponse = await _accountService.GetById(accountId);
+
+                if (directAccountResponse == null)
+                {
+                    return NotFound();
+                }
+                return Ok(directAccountResponse);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("{accountId")]
+        public async Task<IActionResult> Update(int accountId, AccountRequest request)
+        {
+            try
+            {
+                DirectAccountResponse directAccountResponse = await _accountService.Update(accountId, request);
+
+                if (directAccountResponse == null)
+                {
+                    return NotFound();
+                }
+                return Ok(directAccountResponse);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
---
+++ FoodHub Web API/Services/AccountService.cs
@@ -0,0 +1,55 @@
+﻿namespace FoodHub_Web_API.Services
+{
+    public interface IAccountService
+    {
+        Task<List<StaticAccountResponse>> GetAll();
+        Task<DirectAccountResponse> GetById(int accountId);
+        Task<DirectAccountResponse> Update(int accountId, AccountRequest request);
+    }
+
+    public class AccountService
+    {
+
+        private readonly IAccountRepository _accountRepository;
+        private readonly IMapper _mapper;
+
+        public AccountService(IAccountRepository accountRepository, IMapper mapper)
+        {
+            _accountRepository = accountRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<StaticAccountResponse>> GetAll()
+        {
+            List<Account> accounts = await _accountRepository.GetAll();
+
+            if (accounts != null)
+            {
+                return accounts.Select(accounts => _mapper.Map<Account, StaticAccountResponse>(accounts)).ToList();
+            }
+            return null;
+        }
+
+        public async Task<DirectAccountResponse?> GetById(int accountId)
+        {
+            Account account = await _accountRepository.GetById(accountId);
+
+            if (account != null)
+            {
+                return _mapper.Map<DirectAccountResponse>(account);
+            }
+            return null;
+        }
+
+        public async Task<DirectAccountResponse> Update(int accountId, AccountRequest request)
+        {
+            Account account = await _accountRepository.Update(accountId, _mapper.Map<Account>(request));
+
+            if (account != null)
+            {
+                return _mapper.Map<DirectAccountResponse>(account);
+            }
+            return null;
+        }
+    }
+}
